5f447c63ba052b100ce5e4603d4684794a4c1b85
Foyez Ahmed <foyezahmed@foyezs-air.home>
2024-05-29 15:48:53 +0100

    make api/articles endpoint available and response returned an array of article objects

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const { getApi } = require('./controllers/api.controllers')
 const { getTopics } = require('./controllers/topics.controllers');
-const { getArticleById } = require('./controllers/articles.controllers');
+const { getArticleById, getArticles } = require('./controllers/articles.controllers');
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require('./errors');
 
 app.use(express.json());
 
 app.get('/api', getApi)
 app.get('/api/topics', getTopics);
+app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleById);
+
 
 app.all('/*', (req, res) => {
     res.status(404).send({ msg: 'Route not found' });
--- model/articles.models.js
+++ model/articles.models.js
@@ -7,6 +7,24 @@ exports.selectArticleById = (article_id) => {
         [article_id]
     )
     .then((result) => {
-        return result.rows[0];
+        const article = result.rows[0];
+        if (!article) {
+            return Promise.reject({ status: 404, msg: 'Article not found' });
+        }
+        return article;
+    });
+};
+exports.selectAllArticles = () => {
+    return db
+    .query(
+        `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON comments.article_id = articles.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;`
+    )
+    .then((result) => {
+        return result.rows;
     });
 };
