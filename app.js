9b7de69264f06d385add0391ddac3ab57bb3b860
Foyez Ahmed <foyezahmed@foyezs-air.home>
2024-05-29 10:11:46 +0100

    implement endpoint api returning a JSON object

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const { getApi } = require('./controllers/api.controllers')
 const { getTopics } = require('./controllers/topics.controllers');
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require('./errors');
 
 app.use(express.json());
 
+app.get('/api', getApi)
 app.get('/api/topics', getTopics);
 
 app.all('/*', (req, res) => {
